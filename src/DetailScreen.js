3f0af0d36a021ac203b8521d6b11c7d1863e8c05
Yopy Tri Buana <nero555@Yopys-MacBook-Air.local>
2023-04-03 10:06:16 +0700

    pertemuan 13

--- src/DetailScreen.js
+++ src/DetailScreen.js
@@ -1,12 +1,107 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import Video from 'react-native-video'
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const DetailScreen = () => {
+
+const DetailScreen = (props) => {
+    const detail = props.route.params.item;
+    console.log(props.route.params.item.trailer);
     return (
-        <View>
-            <Text>DetailScreen</Text>
+        <View style={styles.container}>
+            <TextInput style={styles.input} placeholder="Search" />
+            <View style={styles.cardMovie}>
+                <Video
+                    source={detail.trailer}
+                    rate={1.0}
+                    volume={10.0}
+                    shouldPlay={true}
+                    positionMilis={0}
+                    resizeMode="contain"
+                    paused={false}
+                    style={{ width: '100%', height: 300 }}
+                />
+                <View style={styles.titleSection}>
+                    <Text style={styles.titleText}>{detail.title}</Text>
+                    <Text style={styles.subTitleText}>{detail.release} | {detail.genre}</Text>
+                </View>
+                <View style={styles.container2}>
+                    <Image source={detail.poster} style={{
+                        height: 200,
+                        width: "35%",
+                        borderRadius: 20,
+                    }} />
+                    <View style={{ flex: 1, flexDirection: 'column' }}>
+                        <View style={{ paddingTop: 10, flexDirection: 'row', marginBottom: 20, justifyContent: 'space-evenly' }}>
+                            <View style={{ flexDirection: 'column', marginRight: 15, justifyContent: 'space-around', }}>
+                                <Icon name="star" size={30} style={{ color: "#FFc200", }} />
+                                <Text>{detail.rating}/10</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <Icon name="staro" size={30} style={{ color: "#ffc200", paddingLeft: 10 }} />
+                                <Text>rate this</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.txtSinopsis}>{detail.sinopsis}</Text>
+                    </View>
+
+                </View>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+    },
+    container2: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        marginBottom: 20,
+    },
+    input: {
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10,
+        marginVertical: 10,
+        marginHorizontal: 10,
+    },
+    cardMovie: {
+        backgroundColor: '#fff',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+    },
+    titleSection: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        borderBottomColor: '#000',
+        borderBottomWidth: 1,
+        paddingBottom: 10,
+        marginBottom: 10,
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    subTitleText: {
+        fontSize: 16,
+        color: '#aaa',
+    },
+    txtSinopsis: {
+        fontSize: 12,
+        paddingLeft: 10,
+        justifyContent: 'space-evenly',
+        marginLeft: 5,
+        textAlign: 'justify',
+    },
+
+})
 
 export default DetailScreen
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -5,61 +5,91 @@ import poster2 from '../assets/poster2.png'
 import poster3 from '../assets/poster3.png'
 import poster4 from '../assets/poster4.png'
 import poster5 from '../assets/poster5.png'
+import poster6 from '../assets/haikyuu.png'
+import poster7 from '../assets/bnha.png'
+import video1 from '../assets/yp.mp4'
+import video2 from '../assets/jjk.mp4'
+import video3 from '../assets/op.mp4'
+import video4 from '../assets/pot.mp4'
+import video5 from '../assets/sxf.mp4'
+import video6 from '../assets/Haikyuu.mp4'
+import video7 from '../assets/bnha.mp4'
 import { useNavigation } from '@react-navigation/native'
 
 const Homepage = () => {
-
     const dataMovie = [
         {
             id: 1,
             title: 'Yowamushi Pedal',
             rating: 8.5,
             genre: 'Anime',
+            release: '2013',
             poster: poster1,
             sinopsis: 'Anime Yowamishi Pedal menceritakan tentang perjalanan tim olahraga balap sepeda, Mereka memiliki bakat yang sangat luar biasa. Persaingan menjadi bagian dari cerita, Kekalahan yang sangat mengerikan dan motivasi yang tumbuh dengan sangat cepat.',
-            trailer: 'https://www.youtube.com/watch?v=3SkUP7wXP8Y'
+            trailer: video1
         },
         {
             id: 2,
             title: 'Jujutsu No Kaisen',
             rating: 8.5,
             genre: 'Anime',
+            release: '2019',
             poster: poster2,
             sinopsis: 'Jujutsu Kaisen bercerita tentang seorang siswa SMA bernama Yuji Itadori yang bergabung dengan organisasi rahasia bernama Jujutsu Sorcerers. Yuji bergabung ke dalam organisasi itu untuk membunuh kutukan kejam bernama Ryomen Sukuna yang ada di tubuhnya.',
-            trailer: 'https://www.youtube.com/watch?v=f7R6NA4Yo00'
+            trailer: video2
         },
         {
             id: 3,
             title: 'One Piece',
             rating: 8.5,
             genre: 'Anime',
+            release: '1999',
             poster: poster3,
             sinopsis: 'One Piece menceritakan perjalanan Luffy, seorang anak laki-laki yang bertemu bajak laut hebat bernama Shanks, dimana Luffy terinspirasi oleh kehebatan Shanks dan bermimpi suatu hari nanti ingin menjadi bajak laut. Kemudian Shanks menjanjikan Luffy untuk reuni di masa depan dan memberikan topi jeraminya.',
-            trailer: 'https://www.youtube.com/watch?v=LHTYpWI3S6Q'
+            trailer: video3
         },
         {
             id: 4,
             title: 'Prince of Tennis',
             rating: 8.5,
             genre: 'Anime',
+            release: '2001',
             poster: poster4,
             sinopsis: 'Anime Prince of Tennis ini bercerita tentang seorang anak lelaki kelas 1 SMP yang memiliki bakat luar biasa dalam tenis. Ia adalah Ryoma Echizen (baca: Echizen Ryoma). Dari episode-episode awal, kita akan disuguhi kehebatan-kehebatan si Ryoma ini. Ya, mirip dengan Tsabasa.',
-            trailer: 'https://www.youtube.com/watch?v=cQkLe1UgDbU'
+            trailer: video4
         },
         {
             id: 5,
             title: 'Spy X Family',
             rating: 8.5,
             genre: 'Anime',
+            release: '2019',
             poster: poster5,
             sinopsis: 'Spy x Family mengisahkan tentang agen mata-mata terampil bernama sandi "Twilight" yang menjalankan Operasi Strix, yakni misi penting terkait perdamaian antarnegara mencakup Westalis di barat dan Ostania di timur.',
-            trailer: 'https://www.youtube.com/watch?v=CCXLUQzuigw'
+            trailer: video5
         },
+        {
+            id: 6,
+            title: 'Haikyuu!!',
+            rating: 8.5,
+            genre: 'Anime',
+            release: '2014',
+            poster: poster6,
+            sinopsis: 'Haikyuu!! menceritakan tentang Shoyo Hinata yang merupakan seorang siswa SMA yang memiliki bakat dalam permainan voli. Namun, karena kecil dan kurus, ia tidak pernah mendapatkan perhatian dari orang lain. Namun, ketika ia melihat pertandingan voli antara tim SMA di sekolahnya, ia terinspirasi untuk bermain voli.',
+            trailer: video6
+        },
+        {
+            id: 7,
+            title: 'Boku No Hero Academia',
+            rating: 8.5,
+            genre: 'Anime',
+            release: '2016',
+            poster: poster7,
+            sinopsis: 'Boku no Hero Academia menceritakan tentang seorang anak laki-laki bernama Izuku Midoriya yang memiliki bakat super yang luar biasa. Namun, karena tidak dilahirkan dengan kekuatan super, Izuku tidak pernah mendapatkan perhatian dari orang lain. Namun, ketika ia melihat pertandingan voli antara tim SMA di sekolahnya, ia terinspirasi untuk bermain voli.',
+            trailer: video7
+        }
     ]
-
     const navigation = useNavigation()
-
-
     return (
         <View style={styles.container}>
             <TextInput style={styles.input} placeholder="Search" />
@@ -144,8 +174,8 @@ const Homepage = () => {
                                 <Image source={item.poster} style={styles.image} />
                                 <Text style={styles.sinopsis}>{item.sinopsis}</Text>
                                 <View style={styles.container6}>
-                                    <Text style={styles.text4}>Rating: {item.rating}</Text>
-                                    <Text style={styles.text4}>Genre: {item.genre}</Text>
+                                    <Text>Rating: {item.rating}</Text>
+                                    <Text>Genre: {item.genre}</Text>
                                 </View>
                             </TouchableOpacity>
                         )
